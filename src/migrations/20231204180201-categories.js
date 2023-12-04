8e9862a53fa0c09bea94d4bc1298f3638829a47c
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-12-04 17:03:13 -0300

    Requisito 01 Ok, criado migrations

---
+++ src/migrations/20231204180201-categories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.createTable('categories',{
+      id: {
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+      },
+
+      name: {
+        type:Sequelize.STRING,
+        allowNull:false,
+      }
+    
+    })
+  },
+
+  async down (queryInterface, _Sequelize) {
+
+    await queryInterface.dropTable('categories',null,{})
+  }
+};
---
+++ src/migrations/20231204183029-posts_categories.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      await queryInterface.createTable('posts_categories', {
+        postId: {
+          type:Sequelize.INTEGER,
+          primaryKey:true,
+          field:'post_id',
+          references: {
+            model: 'blog_posts',
+            key:'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+
+        categoryId: {
+          type:Sequelize.INTEGER,
+          primaryKey:true,
+          field:'category_id',
+          references: {
+            model: 'categories',
+            key:'id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+        },
+
+      })
+  },
+
+  async down (queryInterface, _Sequelize) {
+      await queryInterface.dropTable('posts_categories',null,{})
+  }
+};
