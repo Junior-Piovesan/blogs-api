f74a87d1577b0b86f537dd44c3b55e39ec96b0cb
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-12-04 17:31:46 -0300

    Requisito 02 Ok, criado o model da tabela users

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+const UserModel = (sequelize,DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true,
+    },
+
+     displayName:{
+      type: DataTypes.STRING,
+      allowNull:false,
+      field: 'display_name'
+    },
+
+     email: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique: true,
+    },
+     
+     password: {
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+
+     image: {
+      type:DataTypes.STRING,
+    }
+  },
+  {
+    timestamps:false,
+    tableName: 'users',
+    underscored:true,
+  },)
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'user_id',
+      as: 'userId'
+    })
+  }
+
+  return User
+}
+
+module.exports = UserModel
