52510ece8cd03c20529d19d1b581532337a33656
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-12-07 10:36:54 -0300

    Requisito 12 refatorado adição ao banco de dados com transaction

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,15 +1,12 @@
-// const sequelize = require('sequelize');
-
 const returnErrorResponse = require('../utils/returnErrorStatus');
 const returnSuccessResponse = require('../utils/returnSuccessStatus');
 
 const codeHTTP = require('../utils/HTTPCode');
 
-const { Category, BlogPost, User, PostCategory } = require('../models');
+const { Category, BlogPost, User, PostCategory, sequelize } = require('../models');
 
 const checkRegisteredPost = async (post, { email }) => {
   const { title, content, categoryIds } = post;
-  // const t = await new sequelize.Transaction();
   
   const user = await User
     .findOne({ where: { email }, attributes: { exclude: ['password'] } });
@@ -20,56 +17,19 @@ const checkRegisteredPost = async (post, { email }) => {
   if (categories.includes(null)) {
     return returnErrorResponse(codeHTTP.BAD_REQUEST, 'one or more "categoryIds" not found');
   }
+
+  const response = await sequelize.transaction(async (t) => {
+    const createdPost = await BlogPost
+      .create({
+        title, content, userId: user.dataValues.id, published: new Date() }, { transation: t });
   
-  const createdPost = await BlogPost.create(
-    { title, content, userId: user.dataValues.id, published: new Date() },
-  );
+    await Promise.all(categoryIds.map((category) => PostCategory
+      .create({ postId: createdPost.dataValues.id, categoryId: category }, { transaction: t })));
 
-  await Promise.all(categoryIds.map((category) => PostCategory.create(
-    { postId: createdPost.dataValues.id, categoryId: category },
-  )));
+    return createdPost;
+  });
 
-  return returnSuccessResponse(codeHTTP.CREATED, createdPost);
-
-  // const response = await sequelize.Transaction(async (t) => {
-  //   const createdPost = await BlogPost
-  //     .create({ title, content, userId: id, published: new Date() }, { transation: t });
-  
-  //   await Promise.all(categoryIds.map((category) => PostCategory
-  //     .create({ postId: createdPost.dataValues.id, categoryId: category }, { transaction: t })));
-
-  //   return createdPost;
-  // });
-
-  // return returnSuccessResponse(codeHTTP.CREATED, response);
-
-  //   const createdPost = await BlogPost
-  //     .create(
-  //       { title, content, userId: id, published: new Date() },
-  //       { transaction: t },
-  //     );
-  
-  // try {
-  //   const createdPost = await BlogPost
-  //     .create(
-  //       { title, content, userId: id, published: new Date() },
-  //       { transaction: t },
-  //     );
-
-  //   await Promise.all(categoryIds.map((category) => PostCategory
-  //     .create(
-  //       { postId: createdPost.dataValues.id, categoryId: category },
-  //       { transaction: t },
-  //     )));
-
-  //   await t.commit();
-      
-  //   return returnSuccessResponse(codeHTTP.CREATED, createdPost);
-  // } catch (error) {
-  //   console.log(error.message);
-  //   await t.rollback();
-  //   throw e;
-  // }
+  return returnSuccessResponse(codeHTTP.CREATED, response);
 };
 
 module.exports = {
