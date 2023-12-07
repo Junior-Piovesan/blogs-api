72872ea04f4d1aab8f7c7484395802c81590aa63
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-12-07 16:40:35 -0300

    Corrigido erros de linter

--- src/utils/checkPostBelongsUser.js
+++
@@ -1,7 +0,0 @@
-const { Category, BlogPost, User } = require('../models');
-
-const getPost = require('./getPost');
-
-const checkPostBelongsUser = async (id) => {
-  const postToUpdate = await getPost.byId(id, { BlogPost, User, Category });
-};
